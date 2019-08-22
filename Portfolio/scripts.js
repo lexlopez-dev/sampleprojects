df72fd5515f345cfae9673b31f588157ed35b3ab
lexlopez-dev <a.lopezcr9@gmail.com>
2019-08-21 22:17:12 -0700

    updated portfolio

---
+++ Portfolio/scripts.js
@@ -0,0 +1,12 @@
+//Function expression to select elements
+
+const selectElement = s => document.querySelector(s);
+//Open the menu on click
+selectElement(s, ".open").addEventListener("click", () => {
+  selectElement(s, ".nav-list").classList.add("active");
+});
+
+//Close the menu on click
+selectElement(s, ".close").addEventListener("click", () => {
+  selectElement(s, ".nav-list").classList.remove(tokens, "active");
+});
