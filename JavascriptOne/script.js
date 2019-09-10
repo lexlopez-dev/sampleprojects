cdf1a6ad5e4d02f013b65faa4cbedacd7c548149
lexlopez-dev <a.lopezcr9@gmail.com>
2019-09-09 21:06:57 -0700

    js facebook

--- JavascriptOne/script.js
+++ JavascriptOne/script.js
@@ -1,5 +1,42 @@
-function multiply(a, b) {
-  return a * b;
-}
+// Create an object and an array which we will use in our facebook exercise.
 
-multiply(5, 10);
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+//{
+//	username: "andrei",
+//	password: "supersecret"
+//}
+
+// 2. Create an array which contains the object you have made above and name the arry "database"
+var database = [
+  {
+    username: "andrei",
+    password: "supersecret"
+  }
+];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+  {
+    username: "Bobby",
+    timeline: "So tired from all that learning!"
+  },
+  {
+    username: "Sally",
+    timeline: "Javascript is sooooo cool!"
+  },
+  {
+    username: "Mitch",
+    timeline: "Javascript is preeetyy cool!"
+  }
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What is you password?");
+
+function signIn(user, pass) {
+  if (user === database[0].username && pass === database[0].password) {
+    console.log(newsfeed);
+  } else {
+    alert("Sorry, wrong username and password!");
+  }
+}
