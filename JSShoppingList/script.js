e38a76f3fa4ec8bde397c15bb8cce2ae737f9424
lexlopez-dev <a.lopezcr9@gmail.com>
2019-09-18 19:59:08 -0700

    update

--- JSShoppingList/script.js
+++ JSShoppingList/script.js
@@ -3,26 +3,26 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
 function inputLength() {
-	return input.value.length;
+  return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+  if (inputLength() > 0) {
+    createListElement();
+  }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
 }
 
 button.addEventListener("click", addListAfterClick);
