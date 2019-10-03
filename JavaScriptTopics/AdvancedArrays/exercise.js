6a08e69eaf520d8b0797d66d3ebc9cfeaeebf962
lexlopez-dev <a.lopezcr9@gmail.com>
2019-10-02 22:00:21 -0700

    js

---
+++ JavaScriptTopics/AdvancedArrays/exercise.js
@@ -0,0 +1,66 @@
+// Complete the below questions using this array:
+const array = [
+  {
+    username: "john",
+    team: "red",
+    score: 5,
+    items: ["ball", "book", "pen"]
+  },
+  {
+    username: "becky",
+    team: "blue",
+    score: 10,
+    items: ["tape", "backpack", "pen"]
+  },
+  {
+    username: "susy",
+    team: "red",
+    score: 55,
+    items: ["ball", "eraser", "pen"]
+  },
+  {
+    username: "tyson",
+    team: "green",
+    score: 1,
+    items: ["book", "pen"]
+  }
+];
+
+//Create an array using forEach that has all the usernames with a "!" to each of the usernames
+
+const userUrgent = [];
+const forArray = array.forEach(user => {
+  let { username } = user;
+  username = username + "!";
+  userUrgent.push(username);
+});
+
+console.log(userUrgent);
+//Create an array using map that has all the usernames with a "? to each of the usernames
+
+const mapArray = array.map(user => {
+  let { username } = user;
+  username = username + "?";
+  return username;
+});
+
+console.log("map", mapArray);
+
+//Filter the array to only include users who are on team: red
+
+const filterArray = array.filter(users => {
+  let { team } = users;
+});
+
+//Find out the total score of all users using reduce
+
+// (1), what is the value of i?
+// (2), Make this map function pure:
+const arrayNum = [1, 2, 4, 5, 8, 9];
+const newArray = arrayNum.map((num, i) => {
+  console.log(num, i);
+  alert(num);
+  return num * 2;
+});
+
+//BONUS: create a new list with all user information, but add "!" to the end of each items they own.
